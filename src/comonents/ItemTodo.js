82f1f20946db7b1a2f1620600709b97071df0c17
Ivan1906 <yanik.ivan@ukr.net>
2019-05-02 13:43:14 +0300

    Add ItemTodo

--- src/comonents/ItemTodo.js
+++ src/comonents/ItemTodo.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class ItemTodo extends Component {
@@ -7,44 +7,39 @@ export default class ItemTodo extends Component {
   }
 
   render() {
-    let {todo, onChange, onDelete} = this.props;
-    console.log('render = ', todo.text);
+    let { todo, onChange, onDelete } = this.props;
+
     return (
-      <div className="todo" key={todo.id} id={todo.id}>
+      <div className="todo" id={todo.id}>
         <input
           type="checkbox"
           onClick={onChange}
-          defaultChecked={todo.type === 'completed'
-          ? true
-          : false}/>
+          defaultChecked={todo.type === 'completed' ? true : false}
+        />
 
         <span>{todo.text}</span>
         <button onClick={onDelete}>Delete</button>
       </div>
     );
-  };
-};
+  }
+}
 
 ItemTodo.defaultProps = {
   todo: {
     id: 0,
     text: 'No todo element',
-    type: 'new'
+    type: 'new',
   },
   onChange: () => console.log('Missing parameter onChange!'),
-  onDelete: () => console.log('Missing parameter onDelete!')
+  onDelete: () => console.log('Missing parameter onDelete!'),
 };
 
 ItemTodo.propTypes = {
-  todo: PropTypes
-    .shape({
+  todo: PropTypes.shape({
     id: PropTypes.number.isRequired,
     text: PropTypes.string.isRequired,
-    type: PropTypes
-      .oneOf(['new', 'completed'])
-      .isRequired
-  })
-    .isRequired,
+    type: PropTypes.oneOf(['new', 'completed']).isRequired,
+  }).isRequired,
   onChange: PropTypes.func.isRequired,
-  onDelete: PropTypes.func.isRequired
-}
+  onDelete: PropTypes.func.isRequired,
+};
--- src/comonents/ListTodos.js
+++ src/comonents/ListTodos.js
@@ -2,21 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ItemTodo from './ItemTodo';
 
-export const ListTodos = ({match, todos, onChange, onDelete}) => {
+export const ListTodos = ({ match, todos, onChange, onDelete }) => {
   if (match.path === '/new') {
-    todos = todos.filter(todo => (todo.type === 'new'
-      ? todo
-      : null));
+    todos = todos.filter(todo => (todo.type === 'new' ? todo : null));
   } else if (match.path === '/completed') {
-    todos = todos.filter(todo => (todo.type === 'completed'
-      ? todo
-      : null));
+    todos = todos.filter(todo => (todo.type === 'completed' ? todo : null));
   }
 
   return (
     <React.Fragment>
       {todos.map(todo => {
-        return (<ItemTodo key={todo.id} todo={todo} onChange={onChange} onDelete={onDelete}/>);
+        return <ItemTodo key={todo.id} todo={todo} onChange={onChange} onDelete={onDelete} />;
       })}
     </React.Fragment>
   );
@@ -25,19 +21,17 @@ export const ListTodos = ({match, todos, onChange, onDelete}) => {
 ListTodos.defaultProps = {
   todos: [],
   onChange: () => console.log('Missing parameter onChange!'),
-  onDelete: () => console.log('Missing parameter onDelete!')
+  onDelete: () => console.log('Missing parameter onDelete!'),
 };
 
 ListTodos.propTypes = {
-  todos: PropTypes
-    .arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    text: PropTypes.string.isRequired,
-    type: PropTypes
-      .oneOf(['new', 'completed'])
-      .isRequired
-  }))
-    .isRequired,
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      type: PropTypes.oneOf(['new', 'completed']).isRequired,
+    }),
+  ).isRequired,
   onChange: PropTypes.func.isRequired,
-  onDelete: PropTypes.func.isRequired
+  onDelete: PropTypes.func.isRequired,
 };
